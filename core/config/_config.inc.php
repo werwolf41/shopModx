ee20519b3c1df9f374b425733e59dd947d197464
Krasnikov Alexandr <werwolf41@gmail.com>
2017-08-14 22:49:40 +0300

    new progect

---
+++ core/config/_config.inc.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *  MODX Configuration file
+ */
+$database_type = 'mysql';
+$database_server = 'localhost';
+$database_user = 'root';
+$database_password = 'root';
+$database_connection_charset = 'utf8';
+$dbase = 'shop';
+$table_prefix = 'shop_';
+$database_dsn = 'mysql:host=localhost;dbname=shop;charset=utf8';
+$config_options = array (
+);
+$driver_options = array (
+);
+
+$lastInstallTime = 1502739882;
+
+$site_id = 'modx5991fdaaef67c9.09765526';
+$site_sessionname = 'SN5991fd0194f64';
+$https_port = '443';
+$uuid = 'a739b327-10d7-4acd-92a3-2203d884b830';
+
+if (!defined('MODX_CORE_PATH')) {
+    $modx_core_path= '/home/werwolf4/www/colorshop/core/';
+    define('MODX_CORE_PATH', $modx_core_path);
+}
+if (!defined('MODX_PROCESSORS_PATH')) {
+    $modx_processors_path= '/home/werwolf4/www/colorshop/core/model/modx/processors/';
+    define('MODX_PROCESSORS_PATH', $modx_processors_path);
+}
+if (!defined('MODX_CONNECTORS_PATH')) {
+    $modx_connectors_path= '/home/werwolf4/www/colorshop/public_html/connectors/';
+    $modx_connectors_url= '/connectors/';
+    define('MODX_CONNECTORS_PATH', $modx_connectors_path);
+    define('MODX_CONNECTORS_URL', $modx_connectors_url);
+}
+if (!defined('MODX_MANAGER_PATH')) {
+    $modx_manager_path= '/home/werwolf4/www/colorshop/public_html/manager/';
+    $modx_manager_url= '/manager/';
+    define('MODX_MANAGER_PATH', $modx_manager_path);
+    define('MODX_MANAGER_URL', $modx_manager_url);
+}
+if (!defined('MODX_BASE_PATH')) {
+    $modx_base_path= '/home/werwolf4/www/colorshop/public_html/';
+    $modx_base_url= '/';
+    define('MODX_BASE_PATH', $modx_base_path);
+    define('MODX_BASE_URL', $modx_base_url);
+}
+if(defined('PHP_SAPI') && (PHP_SAPI == "cli" || PHP_SAPI == "embed")) {
+    $isSecureRequest = false;
+} else {
+    $isSecureRequest = ((isset ($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) == 'on') || $_SERVER['SERVER_PORT'] == $https_port);
+}
+if (!defined('MODX_URL_SCHEME')) {
+    $url_scheme=  $isSecureRequest ? 'https://' : 'http://';
+    define('MODX_URL_SCHEME', $url_scheme);
+}
+if (!defined('MODX_HTTP_HOST')) {
+    if(defined('PHP_SAPI') && (PHP_SAPI == "cli" || PHP_SAPI == "embed")) {
+        $http_host='colorshop.loc';
+        define('MODX_HTTP_HOST', $http_host);
+    } else {
+        $http_host= array_key_exists('HTTP_HOST', $_SERVER) ? htmlspecialchars($_SERVER['HTTP_HOST'], ENT_QUOTES) : 'colorshop.loc';
+        if ($_SERVER['SERVER_PORT'] != 80) {
+            $http_host= str_replace(':' . $_SERVER['SERVER_PORT'], '', $http_host); // remove port from HTTP_HOST
+        }
+        $http_host .= ($_SERVER['SERVER_PORT'] == 80 || $isSecureRequest) ? '' : ':' . $_SERVER['SERVER_PORT'];
+        define('MODX_HTTP_HOST', $http_host);
+    }
+}
+if (!defined('MODX_SITE_URL')) {
+    $site_url= $url_scheme . $http_host . MODX_BASE_URL;
+    define('MODX_SITE_URL', $site_url);
+}
+if (!defined('MODX_ASSETS_PATH')) {
+    $modx_assets_path= '/home/werwolf4/www/colorshop/public_html/assets/';
+    $modx_assets_url= '/assets/';
+    define('MODX_ASSETS_PATH', $modx_assets_path);
+    define('MODX_ASSETS_URL', $modx_assets_url);
+}
+if (!defined('MODX_LOG_LEVEL_FATAL')) {
+    define('MODX_LOG_LEVEL_FATAL', 0);
+    define('MODX_LOG_LEVEL_ERROR', 1);
+    define('MODX_LOG_LEVEL_WARN', 2);
+    define('MODX_LOG_LEVEL_INFO', 3);
+    define('MODX_LOG_LEVEL_DEBUG', 4);
+}
+if (!defined('MODX_CACHE_DISABLED')) {
+    $modx_cache_disabled= false;
+    define('MODX_CACHE_DISABLED', $modx_cache_disabled);
+}
